c9744cad1e8d8d6c8a1b11dc257af367f8c822b0
Võ Văn Quang <quangvo5803@gmail.com>
2025-06-29 10:24:28 +0700

    [QuangVV - 2025-06-29] Refactor Auto Mapper

--- HomeCareDN/BusinessLogic/DTOs/Application/ContractorApplication/ContractorApplicationCreateRequestDto.cs
+++ HomeCareDN/BusinessLogic/DTOs/Application/ContractorApplication/ContractorApplicationCreateRequestDto.cs
@@ -13,7 +13,6 @@ namespace BusinessLogic.DTOs.Application.ContractorApplication
         public required Guid ServiceRequestID { get; set; }
         public string? Description { get; set; }
         public double EstimatePrice { get; set; }
-        public ApplicationStatus? Status { get; set; } = ApplicationStatus.Pending;
         public List<IFormFile>? Images { get; set; }
     }
 }
--- HomeCareDN/HomeCareDNAPI/Mapping/AutoMapperProfile.cs
+++ HomeCareDN/HomeCareDNAPI/Mapping/AutoMapperProfile.cs
@@ -11,197 +11,55 @@ namespace HomeCareDNAPI.Mapping
     {
         public AutoMapperProfile()
         {
-            //ServiceRequest Create
-            CreateMap<ServiceRequest, ServiceRequestCreateRequestDto>()
-                .ForMember(dest => dest.Images, opt => opt.Ignore());
+            // Enum to string mapping
+            CreateMap<ServiceType, string>()
+                .ConvertUsing(src => src.GetDisplayName());
+            CreateMap<PackageOption, string>().ConvertUsing(src => src.GetDisplayName());
+            CreateMap<BuildingType, string>().ConvertUsing(src => src.GetDisplayName());
+            CreateMap<MainStructureType, string>().ConvertUsing(src => src.GetDisplayName());
+            CreateMap<DesignStyle, string>().ConvertUsing(src => src.GetDisplayName());
+            CreateMap<ApplicationStatus, string>().ConvertUsing(src => src.GetDisplayName());
 
-            CreateMap<ServiceRequestCreateRequestDto, ServiceRequest>()
+            // ServiceRequest
+            CreateMap<ServiceRequest, ServiceRequestDto>()
                 .ForMember(
-                    dest => dest.ServiceType,
-                    opt => opt.MapFrom(src => (ServiceType)src.ServiceType)
-                )
-                .ForMember(
-                    dest => dest.PackageOption,
+                    dest => dest.ImageUrls,
                     opt =>
                         opt.MapFrom(src =>
-                            src.PackageOption.HasValue
-                                ? (PackageOption)src.PackageOption.Value
-                                : (PackageOption?)null
+                            src.Images != null
+                                ? src.Images.Select(i => i.ImageUrl).ToList()
+                                : new List<string>()
                         )
-                )
-                .ForMember(
-                    dest => dest.BuildingType,
-                    opt => opt.MapFrom(src => (BuildingType)src.BuildingType)
-                )
-                .ForMember(
-                    dest => dest.MainStructureType,
-                    opt => opt.MapFrom(src => (MainStructureType)src.MainStructureType)
-                )
-                .ForMember(
-                    dest => dest.DesignStyle,
-                    opt => opt.MapFrom(src => (DesignStyle)src.DesignStyle)
-                )
-                .ForMember(dest => dest.Images, opt => opt.Ignore());
-            // ServiceRequest Update
-            CreateMap<ServiceRequest, ServiceRequestUpdateRequestDto>()
+                );
+
+            CreateMap<ServiceRequestCreateRequestDto, ServiceRequest>()
                 .ForMember(dest => dest.Images, opt => opt.Ignore());
 
             CreateMap<ServiceRequestUpdateRequestDto, ServiceRequest>()
-                .ForMember(
-                    dest => dest.ServiceType,
-                    opt => opt.MapFrom(src => (ServiceType)src.ServiceType)
-                )
-                .ForMember(
-                    dest => dest.PackageOption,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.PackageOption.HasValue
-                                ? (PackageOption)src.PackageOption.Value
-                                : (PackageOption?)null
-                        )
-                )
-                .ForMember(
-                    dest => dest.BuildingType,
-                    opt => opt.MapFrom(src => (BuildingType)src.BuildingType)
-                )
-                .ForMember(
-                    dest => dest.MainStructureType,
-                    opt => opt.MapFrom(src => (MainStructureType)src.MainStructureType)
-                )
-                .ForMember(
-                    dest => dest.DesignStyle,
-                    opt => opt.MapFrom(src => (DesignStyle)src.DesignStyle)
-                )
                 .ForMember(dest => dest.Images, opt => opt.Ignore());
 
-            // ContractorApplication Create
-            CreateMap<ContractorApplication, ContractorApplicationCreateRequestDto>()
-                .ForMember(dest => dest.Images, opt => opt.Ignore());
+            // ContractorApplication
+            CreateMap<ContractorApplication, ContractorApplicationDto>()
+                .ForMember(
+                    dest => dest.ImageUrls,
+                    opt =>
+                        opt.MapFrom(src =>
+                            src.Images != null
+                                ? src.Images.Select(i => i.ImageUrl).ToList()
+                                : new List<string>()
+                        )
+                );
+
             CreateMap<ContractorApplicationCreateRequestDto, ContractorApplication>()
                 .ForMember(dest => dest.Status, opt => opt.Ignore())
                 .ForMember(dest => dest.Images, opt => opt.Ignore());
 
-            // ContractorApplication Update
-            CreateMap<ContractorApplication, ContractorApplicationUpdateRequestDto>()
-                .ForMember(dest => dest.Images, opt => opt.Ignore());
-
             CreateMap<ContractorApplicationUpdateRequestDto, ContractorApplication>()
-                .ForMember(
-                    dest => dest.Status,
-                    opt => opt.MapFrom(src => (ApplicationStatus)src.Status)
-                )
                 .ForMember(dest => dest.Images, opt => opt.Ignore());
 
-            //Service Create
-            CreateMap<Service, ServiceCreateRequestDto>()
-                .ForMember(dest => dest.Images, opt => opt.Ignore());
-
-            CreateMap<ServiceCreateRequestDto, Service>()
-                .ForMember(
-                    dest => dest.ServiceType,
-                    opt => opt.MapFrom(src => (ServiceType)src.ServiceType)
-                )
-                .ForMember(
-                    dest => dest.PackageOption,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.PackageOption.HasValue
-                                ? (PackageOption)src.PackageOption.Value
-                                : (PackageOption?)null
-                        )
-                )
-                .ForMember(
-                    dest => dest.BuildingType,
-                    opt => opt.MapFrom(src => (BuildingType)src.BuildingType)
-                )
-                .ForMember(dest => dest.Images, opt => opt.Ignore());
-
-            //Service Update
-            CreateMap<Service, ServiceUpdateRequestDto>()
-                .ForMember(dest => dest.Images, opt => opt.Ignore())
-                .ReverseMap();
-
-            CreateMap<ServiceUpdateRequestDto, Service>()
-                .ForMember(
-                    dest => dest.ServiceType,
-                    opt => opt.MapFrom(src => (ServiceType)src.ServiceType)
-                )
-                .ForMember(
-                    dest => dest.PackageOption,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.PackageOption.HasValue
-                                ? (PackageOption)src.PackageOption.Value
-                                : (PackageOption?)null
-                        )
-                )
-                .ForMember(
-                    dest => dest.BuildingType,
-                    opt => opt.MapFrom(src => (BuildingType)src.BuildingType)
-                )
-                .ForMember(dest => dest.Images, opt => opt.Ignore());
-
-            // Complex mapping (Response)
-            //ServiceRequest
-            CreateMap<ServiceRequest, ServiceRequestDto>()
-                .ForMember(
-                    dest => dest.ServiceType,
-                    opt => opt.MapFrom(src => src.ServiceType.GetDisplayName())
-                )
-                .ForMember(
-                    dest => dest.PackageOption,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.PackageOption.HasValue
-                                ? src.PackageOption.Value.GetDisplayName()
-                                : null
-                        )
-                )
-                .ForMember(
-                    dest => dest.BuildingType,
-                    opt => opt.MapFrom(src => src.BuildingType.GetDisplayName())
-                )
-                .ForMember(
-                    dest => dest.MainStructureType,
-                    opt => opt.MapFrom(src => src.MainStructureType.GetDisplayName())
-                )
-                .ForMember(
-                    dest => dest.DesignStyle,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.DesignStyle != null ? src.DesignStyle.GetDisplayName() : null
-                        )
-                )
-                .ForMember(
-                    dest => dest.ImageUrls,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.Images != null
-                                ? src.Images.Select(i => i.ImageUrl).ToList()
-                                : new List<string>()
-                        )
-                );
-
-            //Service
+            // Service
             CreateMap<Service, ServiceDto>()
                 .ForMember(
-                    dest => dest.ServiceType,
-                    opt => opt.MapFrom(src => src.ServiceType.GetDisplayName())
-                )
-                .ForMember(
-                    dest => dest.PackageOption,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.PackageOption.HasValue
-                                ? src.PackageOption.Value.GetDisplayName()
-                                : null
-                        )
-                )
-                .ForMember(
-                    dest => dest.BuildingType,
-                    opt => opt.MapFrom(src => src.BuildingType.GetDisplayName())
-                )
-                .ForMember(
                     dest => dest.ImageUrls,
                     opt =>
                         opt.MapFrom(src =>
@@ -211,21 +69,11 @@ namespace HomeCareDNAPI.Mapping
                         )
                 );
 
-            //ContractorApplication
-            CreateMap<ContractorApplication, ContractorApplicationDto>()
-                .ForMember(
-                    dest => dest.Status,
-                    opt => opt.MapFrom(src => src.Status.GetDisplayName())
-                )
-                .ForMember(
-                    dest => dest.ImageUrls,
-                    opt =>
-                        opt.MapFrom(src =>
-                            src.Images != null
-                                ? src.Images.Select(i => i.ImageUrl).ToList()
-                                : new List<string>()
-                        )
-                );
+            CreateMap<ServiceCreateRequestDto, Service>()
+                .ForMember(dest => dest.Images, opt => opt.Ignore());
+
+            CreateMap<ServiceUpdateRequestDto, Service>()
+                .ForMember(dest => dest.Images, opt => opt.Ignore());
         }
     }
 }
