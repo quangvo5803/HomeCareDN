d24ba544efc4b48facdd322bdbfcee316d841948
Võ Văn Quang <quangvo5803@gmail.com>
2025-08-15 10:00:12 +0700

    [QuangVV - 2025-08-16] Install Tailwind

---
+++ homecaredn-client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0d6efd', // similar to Bootstrap primary
+      },
+      container: {
+        center: true,
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1320px',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
