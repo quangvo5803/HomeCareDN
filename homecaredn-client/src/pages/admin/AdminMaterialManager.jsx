7c5ad695f7a2668f07712b524671cecfc9e3daca
DESKTOP-HDH4CS0\DELL <huyltmdev2003@gmail.com>
2025-10-01 17:13:33 +0700

    [HuyLTM - 2025-10-1] Fix sonar

--- homecaredn-client/src/pages/admin/AdminMaterialManager.jsx
+++ homecaredn-client/src/pages/admin/AdminMaterialManager.jsx
@@ -123,9 +123,9 @@ export default function AdminMaterialManager() {
       toast.success(t('SUCCESS.MATERIAL_ADD'));
 
       const newTotal = (totalMaterials ?? 0) + 1;
-      const newLastPage = Math.ceil(newTotal / pageSize);
+      const newLastPage = Math.ceil(newTotal / pageSize) || 1;
 
-      if (newLastPage !== currentPage) {
+      if (newLastPage === currentPage) {
         await fetchMaterials({ PageNumber: currentPage, PageSize: pageSize });
       } else {
         setCurrentPage(newLastPage);
