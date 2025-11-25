d9f15db7b789d35eef82eb2c1cc0d5daa16b6098
haithanh019 <haithanh019@gmail.com>
2025-11-25 17:30:36 +0700

    [ThanhHH - 2025-11-25] Update Chat Section For Material

--- HomeCareDN/BusinessLogic/DTOs/Application/Chat/User/Convesation/ConversationDto.cs
+++ HomeCareDN/BusinessLogic/DTOs/Application/Chat/User/Convesation/ConversationDto.cs
@@ -5,12 +5,14 @@
         public Guid ConversationID { get; set; }
         public string? CustomerID { get; set; }
         public string? ContractorID { get; set; }
+        public string? DistributorID { get; set; }
         public string? AdminID { get; set; }
         public string? UserID { get; set; }
         public string? UserEmail { get; set; }
         public string? UserName { get; set; }
         public string? UserRole { get; set; }
         public Guid? ServiceRequestID { get; set; }
+        public Guid? MaterialRequestID { get; set; }
         public bool IsAdminRead { get; set; }
         public int AdminUnreadMessageCount { get; set; }
         public required string ConversationType { get; set; }
