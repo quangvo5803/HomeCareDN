5d5b5a848bf4f0e03e0f33f5c6fb8ac42da443df
Quang Võ <quangvo5803@gmail.com>
2025-12-22 22:19:54 +0700

    [QuangVV - 2025/12/22] Fix Bug Realtime

--- HomeCareDN/HomeCareDNAPI/Program.cs
+++ HomeCareDN/HomeCareDNAPI/Program.cs
@@ -85,24 +85,6 @@ namespace HomeCareDNAPI
                         ValidAudience = builder.Configuration["Jwt:Audience"],
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
                     };
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnMessageReceived = context =>
-                        {
-                            var accessToken = context.Request.Query["access_token"];
-                            var path = context.HttpContext.Request.Path;
-
-                            if (
-                                !string.IsNullOrEmpty(accessToken)
-                                && path.StartsWithSegments("/hubs")
-                            )
-                            {
-                                context.Token = accessToken;
-                            }
-
-                            return Task.CompletedTask;
-                        },
-                    };
                 });
             builder.Services.AddCors(options =>
             {
